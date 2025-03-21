fb6dafda189d615bbc131f52330cedefb14b5c23
Benoit Thery <thery.benoit@hotmail.fr>
2025-03-21 12:27:16 +0100

    Note et BestRated - Final

--- backend/controllers/books.js
+++ backend/controllers/books.js
@@ -6,9 +6,6 @@ exports.createBook = (req, res, next) => {
   const newBook = new Book({
     ...newBookObject,
     imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
-    ratings: newBookObject.ratings,
-    averageRating: newBookObject.ratings,									// Vérifier cette ligne
-    userId: req.auth.userId
   })
   newBook.save()
     .then(books => res.status(200).json("Livre ajouté avec succès !"))
@@ -27,7 +24,7 @@ exports.getBook = (req, res, next) => {
     .catch(error => res.status(400).json({ error }));
 };
 
-exports.deleteBook = (req, res, next) => {
+exports.deleteBook = (req, res, next) => {                                          // Actuellement, les images ne sont pas supprimées du dossier de backend à la suppression
   Book.findOne({_id: req.params.id})
     .then(book => {
       if (book.userId != req.auth.userId) {
@@ -70,25 +67,25 @@ exports.modifyBook = (req, res, next) => {
     })})
   .catch((error) => res.status(401).json({message: {error}}))};
 
-exports.rateBook = (req, res, next) => {
+exports.rateBook = (req, res, next) => {                      
   Book.findOne({_id: req.params.id})
   .then((book) => {
-    console.log(book);
-    console.log(req.params.id);
     Book.updateOne({_id: req.params.id}, {
         $push: {ratings: {userId: req.body.userId, grade: req.body.rating}},
         $set: {averageRating: (
           (req.body.rating + (book.ratings?.reduce((sum, r) => sum + r.grade, 0) || 0)) / ((book.ratings?.length || 0) +1)
         )}
         })
-    .then(() => res.status(200).json({message: "Note prise en compte!"}))
+    .then(() => res.status(200).json({message: "Note prise en compte!"}))           // Actuellement, ça renvoit un id: undefined en console, l'erreur vient probablement de là
     .catch((error) => res.status(401).json({message: {error}}))
   })
   .catch((error) => res.status(401).json({message: {error}}));
 };
   
 exports.getBestRated = (req, res, next) => {
-  Book.find().sort({averageRating: -1}).limit(3)
-    .then((books) => res.status(200).json(books))
+  Book.find().sort({averageRating: -1, year: -1}).limit(3)                              //Si deux livres ont EXACTEMENT la même note moyenne, on choisit le plus récent
+    .then((books) => {
+      res.status(200).json(books)
+    })
     .catch(error => res.status(400).json({ error }));
 };
--- backend/models/Book.js
+++ backend/models/Book.js
@@ -10,7 +10,7 @@ const bookSchema = mongoose.Schema({
     userId: { type: String, required: true },
     grade: { type: Number, required: true }
   }],
-  averageRating: { type: Number },
+  averageRating: { type: Number, required: true },
   userId: { type: String, required: true }
 });
 
